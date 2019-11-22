5bd0161d876c31043192777c9b4dc16aa7dbd2dd
John Oliver <1615532+johnoliver@users.noreply.github.com>
2019-11-22 22:35:10 +0000

    add source field to release

---
+++ adoptopenjdk-api-v3-models/src/main/kotlin/net/adoptopenjdk/api/v3/models/SourcePackage.kt
@@ -0,0 +1,24 @@
+package net.adoptopenjdk.api.v3.models
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema
+
+class SourcePackage {
+
+    @Schema(example = "OpenJDK8U-sources_8u232b09.tar.gz")
+    val name: String
+
+    @Schema(example = "https://github.com/AdoptOpenJDK/openjdk8-upstream-binaries/releases/download/jdk8u232-b09/OpenJDK8U-sources_8u232b09.tar.gz")
+    val link: String
+
+    @Schema(example = "82573385")
+    val size: Long
+
+    constructor(
+            name: String,
+            link: String,
+            size: Long) {
+        this.name = name
+        this.link = link
+        this.size = size
+    }
+}
