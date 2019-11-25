73718d6fc18fdf8a006b1f713c29f636b020487f
John Oliver <1615532+johnoliver@users.noreply.github.com>
2019-11-25 16:18:15 +0000

    Default upstream assets to jdk image type

--- adoptopenjdk-api-v3-updater/src/test/kotlin/net/adoptopenjdk/api/UpstreamBinaryMapperTest.kt
+++ adoptopenjdk-api-v3-updater/src/test/kotlin/net/adoptopenjdk/api/UpstreamBinaryMapperTest.kt
@@ -3,38 +3,60 @@ package net.adoptopenjdk.api
 import kotlinx.coroutines.runBlocking
 import net.adoptopenjdk.api.v3.dataSources.github.graphql.models.GHAsset
 import net.adoptopenjdk.api.v3.mapping.upstream.UpstreamBinaryMapper
+import net.adoptopenjdk.api.v3.models.ImageType
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 
 class UpstreamBinaryMapperTest {
 
+    private fun getAssetList(names: List<String>): List<GHAsset> {
+        return names.map { name ->
+            GHAsset(
+                    name,
+                    1L,
+                    "",
+                    1L,
+                    "2013-02-27T19:35:32Z")
+        }
+    }
+
+
     @Test
     fun filtersOutDebugInfo() {
-
-        val jdk = GHAsset(
+        val assets = getAssetList(listOf(
                 "OpenJDK8U-jdk_x64_linux_8u232b09.tar.gz",
-                1L,
-                "",
-                1L,
-                "2013-02-27T19:35:32Z")
-
-        val assets = listOf(GHAsset(
-                "OpenJDK8U-jdk_x64_linux_8u232b09-debuginfo.tar.gz",
-                1L,
-                "",
-                1L,
-                "2013-02-27T19:35:32Z"),
-                jdk
-        )
-
+                "OpenJDK8U-jdk_x64_linux_8u232b09-debuginfo.tar.gz"
+        ))
         runBlocking {
             val binaryList = UpstreamBinaryMapper.toBinaryList(assets)
 
             assert(binaryList.size == 1)
-            assertEquals(jdk.name, binaryList.get(0).`package`!!.name)
+            assertEquals(assets.get(0).name, binaryList.get(0).`package`!!.name)
+        }
+    }
+
+    @Test
+    fun correctlyClassifiesImageType() {
+
+        val assets = getAssetList(listOf(
+                "OpenJDK11U-x64_linux_11.0.3_7.tar.gz",
+                "OpenJDK11U-x64_windows_11.0.3_6_ea.zip",
+                "OpenJDK11U-testimage_x64_linux_11.0.5_10.tar.gz",
+                "OpenJDK11U-jre_aarch64_linux_11.0.5_10.tar.gz",
+                "OpenJDK11U-jdk_aarch64_linux_11.0.5_10-debuginfo.tar.gz",
+                "OpenJDK11U-sources_11.0.5_10.tar.gz"
+        ))
+
+        runBlocking {
+            val binaryList = UpstreamBinaryMapper.toBinaryList(assets)
+
+            assertEquals(4, binaryList.size)
+            assertEquals(ImageType.jdk, binaryList.get(0).image_type)
+            assertEquals(ImageType.jdk, binaryList.get(1).image_type)
+            assertEquals(ImageType.testimage, binaryList.get(2).image_type)
+            assertEquals(ImageType.jre, binaryList.get(3).image_type)
         }
 
     }
-
 }
