c6f7d83e475298a55f55d4543ca4fb35d5601678
John Oliver <1615532+johnoliver@users.noreply.github.com>
2019-12-20 16:13:33 +0000

    Exclude upstream stats

--- adoptopenjdk-api-v3-frontend/src/main/kotlin/net/adoptopenjdk/api/v3/routes/stats/DownloadStatsResource.kt
+++ adoptopenjdk-api-v3-frontend/src/main/kotlin/net/adoptopenjdk/api/v3/routes/stats/DownloadStatsResource.kt
@@ -7,7 +7,6 @@ import net.adoptopenjdk.api.v3.models.DownloadStats
 import net.adoptopenjdk.api.v3.models.TotalStats
 import org.eclipse.microprofile.openapi.annotations.Operation
 import org.eclipse.microprofile.openapi.annotations.media.Schema
-import org.eclipse.microprofile.openapi.annotations.tags.Tag
 import java.time.LocalDateTime
 import javax.ws.rs.GET
 import javax.ws.rs.Path
@@ -15,7 +14,6 @@ import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 
 
-@Tag(name = "Stats")
 @Path("/stats/downloads")
 @Produces(MediaType.APPLICATION_JSON)
 @Schema(hidden = true)
