e286076206b1c4ca4aa8d6e0d3ec182decabdf70
John Oliver <1615532+johnoliver@users.noreply.github.com>
2019-12-28 21:57:52 +0000

    Limit parallelism

--- adoptopenjdk-api-v3-updater/src/main/kotlin/net/adoptopenjdk/api/v3/HttpClientFactory.kt
+++ adoptopenjdk-api-v3-updater/src/main/kotlin/net/adoptopenjdk/api/v3/HttpClientFactory.kt
@@ -4,13 +4,12 @@ import java.net.http.HttpClient
 import java.time.Duration
 import java.util.concurrent.Executors
 
-
 object HttpClientFactory {
     var client: HttpClient = HttpClient
             .newBuilder()
             .followRedirects(HttpClient.Redirect.ALWAYS)
             .connectTimeout(Duration.ofSeconds(10))
-            .executor(Executors.newFixedThreadPool(6))
+            .executor(Executors.newSingleThreadExecutor())
             .build()
 
     fun getHttpClient(): HttpClient {
