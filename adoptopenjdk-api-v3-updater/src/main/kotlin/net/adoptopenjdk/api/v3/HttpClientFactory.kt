01d8bba4211ad4464caf107d7e270d08164bc034
John Oliver <1615532+johnoliver@users.noreply.github.com>
2019-12-30 16:11:20 +0000

    Set minimum backoff to 10 sec

--- adoptopenjdk-api-v3-updater/src/main/kotlin/net/adoptopenjdk/api/v3/HttpClientFactory.kt
+++ adoptopenjdk-api-v3-updater/src/main/kotlin/net/adoptopenjdk/api/v3/HttpClientFactory.kt
@@ -2,14 +2,12 @@ package net.adoptopenjdk.api.v3
 
 import java.net.http.HttpClient
 import java.time.Duration
-import java.util.concurrent.Executors
 
 object HttpClientFactory {
     var client: HttpClient = HttpClient
             .newBuilder()
             .followRedirects(HttpClient.Redirect.ALWAYS)
             .connectTimeout(Duration.ofSeconds(10))
-            .executor(Executors.newSingleThreadExecutor())
             .build()
 
     fun getHttpClient(): HttpClient {
