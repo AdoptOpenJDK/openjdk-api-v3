9e8c5e9a062ccec7dfa0e823bcde7bddc6920279
John Oliver <1615532+johnoliver@users.noreply.github.com>
2020-01-03 14:45:02 +0000

    Print error

--- adoptopenjdk-api-v3-updater/src/main/kotlin/net/adoptopenjdk/api/v3/HttpClientFactory.kt
+++ adoptopenjdk-api-v3-updater/src/main/kotlin/net/adoptopenjdk/api/v3/HttpClientFactory.kt
@@ -2,10 +2,12 @@ package net.adoptopenjdk.api.v3
 
 import java.net.http.HttpClient
 import java.time.Duration
+import java.util.concurrent.Executors
 
 object HttpClientFactory {
     private var client: HttpClient = HttpClient
             .newBuilder()
+            .executor(Executors.newFixedThreadPool(2))
             .followRedirects(HttpClient.Redirect.NORMAL)
             .connectTimeout(Duration.ofSeconds(30))
             .build()
@@ -13,6 +15,7 @@ object HttpClientFactory {
 
     private var nonRedirect: HttpClient = HttpClient
             .newBuilder()
+            .executor(Executors.newFixedThreadPool(2))
             .followRedirects(HttpClient.Redirect.NEVER)
             .connectTimeout(Duration.ofSeconds(30))
             .build()
