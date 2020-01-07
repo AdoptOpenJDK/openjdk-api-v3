c6a8733c7d3483ec040edfb6690eb12f8bcc31f1
John Oliver <1615532+johnoliver@users.noreply.github.com>
2020-01-07 14:16:04 +0000

    add tracking stats filter (#77)

---
+++ adoptopenjdk-api-v3-models/src/main/kotlin/net/adoptopenjdk/api/v3/models/StatsSource.kt
@@ -0,0 +1,12 @@
+package net.adoptopenjdk.api.v3.models
+
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType
+import org.eclipse.microprofile.openapi.annotations.media.Schema
+
+
+@Schema(type = SchemaType.STRING, defaultValue = "all", enumeration = ["github", "dockerhub", "all"], example = "all")
+enum class StatsSource {
+    github,
+    dockerhub,
+    all;
+}
