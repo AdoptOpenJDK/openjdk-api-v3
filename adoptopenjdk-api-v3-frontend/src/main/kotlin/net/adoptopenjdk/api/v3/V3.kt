f3b250077ec18e59ac5c8cb9ee8e4118f8c485a5
George Adams <georgeadams1995@gmail.com>
2020-03-26 14:05:12 +0000

    add description to top of swagger doc (#131)

--- adoptopenjdk-api-v3-frontend/src/main/kotlin/net/adoptopenjdk/api/v3/V3.kt
+++ adoptopenjdk-api-v3-frontend/src/main/kotlin/net/adoptopenjdk/api/v3/V3.kt
@@ -22,12 +22,14 @@ object ServerConfig {
     const val SERVER = "https://api.adoptopenjdk.net"
 }
 
+const val DESCRIPTION = "<li><strong>NOTICE:</strong> AdoptOpenJDK API v1 Has now been removed.</li><li><strong>NOTICE:</strong> AdoptOpenJDK API v2 Has now been deprecated.</li><li><strong>NOTICE:</strong> If you are using v2 please move to the v3 as soon as possible. Please raise any migration problems as an issue in the <a href=\"https://github.com/AdoptOpenJDK/openjdk-api-v3/issues/new\">v3 issue tracker</a>.</li><li><strong>NOTICE:</strong> For v2 docs please refer to <a href=\"https://api.adoptopenjdk.net/README\">https://api.adoptopenjdk.net/README</a>.</li>"
+
 @OpenAPIDefinition(
         servers = [
             Server(url = ServerConfig.SERVER),
             Server(url = "https://staging-api.adoptopenjdk.net")
         ],
-        info = Info(title = "v3", version = "3.0.0-beta"))
+        info = Info(title = "v3", version = "3.0.0-beta", description = DESCRIPTION))
 @ApplicationPath("/v3")
 class V3 : Application() {
 
