a1f7d7dbb14e68b3803aab49d79e2804413417ba
John Oliver <1615532+johnoliver@users.noreply.github.com>
2020-04-01 18:51:14 +0100

    Pretty print returned json (#149)

--- adoptopenjdk-api-v3-frontend/src/main/kotlin/net/adoptopenjdk/api/v3/V3.kt
+++ adoptopenjdk-api-v3-frontend/src/main/kotlin/net/adoptopenjdk/api/v3/V3.kt
@@ -37,7 +37,7 @@ class V3 : Application() {
     init {
         cors = setOf(object : ContainerResponseFilter {
             override fun filter(requestContext: ContainerRequestContext?, responseContext: ContainerResponseContext) {
-                responseContext.getHeaders().add("Access-Control-Allow-Origin", "*")
+                responseContext.headers.add("Access-Control-Allow-Origin", "*")
             }
         })
 
