6ab2180eaee4a3ce1d4b16c14e8877a184f72cc1
John Oliver <1615532+johnoliver@users.noreply.github.com>
2020-04-04 19:23:44 +0000

    Fix equality of gitIds

--- adoptopenjdk-api-v3-models/src/main/kotlin/net/adoptopenjdk/api/v3/dataSources/models/GithubId.kt
+++ adoptopenjdk-api-v3-models/src/main/kotlin/net/adoptopenjdk/api/v3/dataSources/models/GithubId.kt
@@ -12,4 +12,21 @@ class GithubId {
             githubId: String) {
         this.githubId = githubId.split("\\.")[0]
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as GithubId
+
+        if (githubId != other.githubId) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return githubId.hashCode()
+    }
+
+
 }
