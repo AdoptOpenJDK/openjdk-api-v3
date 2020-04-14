dbad3deae0353b61d61a6ddd0f68f5984056f514
Adam Thorpe <adam.thorpe@ibm.com>
2020-04-14 16:20:06 +0100

    Clean up

--- adoptopenjdk-api-v3-updater/src/test/kotlin/net/adoptopenjdk/api/StatsCalculatorTest.kt
+++ adoptopenjdk-api-v3-updater/src/test/kotlin/net/adoptopenjdk/api/StatsCalculatorTest.kt
@@ -1,44 +1,32 @@
 package net.adoptopenjdk.api
 
-import kotlinx.coroutines.runBlocking
-import net.adoptopenjdk.api.v3.V3Updater
-import net.adoptopenjdk.api.v3.models.JvmImpl
-import org.awaitility.Awaitility
-import org.junit.Ignore
-import org.junit.jupiter.api.Test
-import java.util.concurrent.TimeUnit
-import org.junit.jupiter.api.BeforeAll
-
+import net.adoptopenjdk.api.v3.TimeSource
+import net.adoptopenjdk.api.v3.dataSources.models.AdoptRepos
+import net.adoptopenjdk.api.v3.dataSources.models.FeatureRelease
+import net.adoptopenjdk.api.v3.dataSources.models.Releases
 import net.adoptopenjdk.api.v3.models.Architecture
 import net.adoptopenjdk.api.v3.models.Binary
+import net.adoptopenjdk.api.v3.models.GithubDownloadStatsDbEntry
 import net.adoptopenjdk.api.v3.models.HeapSize
 import net.adoptopenjdk.api.v3.models.ImageType
 import net.adoptopenjdk.api.v3.models.Installer
+import net.adoptopenjdk.api.v3.models.JvmImpl
 import net.adoptopenjdk.api.v3.models.OperatingSystem
 import net.adoptopenjdk.api.v3.models.Package
 import net.adoptopenjdk.api.v3.models.Project
 import net.adoptopenjdk.api.v3.models.Release
-import net.adoptopenjdk.api.v3.dataSources.models.FeatureRelease
-import net.adoptopenjdk.api.v3.dataSources.models.Releases
 import net.adoptopenjdk.api.v3.models.ReleaseType
 import net.adoptopenjdk.api.v3.models.Vendor
-import net.adoptopenjdk.api.v3.TimeSource
 import net.adoptopenjdk.api.v3.models.VersionData
-import net.adoptopenjdk.api.v3.dataSources.models.AdoptRepos
 import net.adoptopenjdk.api.v3.stats.GithubDownloadStatsCalculator
-import net.adoptopenjdk.api.v3.models.GithubDownloadStatsDbEntry
-
-
-import java.time.ZonedDateTime
-
-
+import org.junit.jupiter.api.Test
 
 class StatsCalculatorTest {
 
     @Test
     fun testGithubStatsCalculator() {
         val adoptRepos = AdoptRepos(listOf(generateFeatureRelease()))
-        val result : List<GithubDownloadStatsDbEntry> = GithubDownloadStatsCalculator().getStats(adoptRepos)
+        val result: List<GithubDownloadStatsDbEntry> = GithubDownloadStatsCalculator().getStats(adoptRepos)
 
         assert(result[0].feature_version == 8)
         println(result[0].downloads)
@@ -60,19 +48,19 @@ class StatsCalculatorTest {
                 time,
                 arrayOf(
                     Binary(
-                        Package("a","b",1L,"v","c",12L,"d"),
-                        15L, //Download count
+                        Package("a", "b", 1L, "v", "c", 12L, "d"),
+                        15L, // Download count
                         time,
                         "d",
-                        Installer("a","b",1L,"v","c",3L),
+                        Installer("a", "b", 1L, "v", "c", 3L),
                         HeapSize.normal,
                         OperatingSystem.linux,
                         Architecture.x64,
                         ImageType.jdk,
                         JvmImpl.hotspot,
                         Project.jdk
-                    ), Binary (
-                        Package("a","b",1L,"v","c",250L,"d"),
+                    ), Binary(
+                        Package("a", "b", 1L, "v", "c", 250L, "d"),
                         250L,
                         time,
                         "d",
@@ -83,8 +71,8 @@ class StatsCalculatorTest {
                         ImageType.jdk,
                         JvmImpl.hotspot,
                         Project.jdk
-                    ), Binary (
-                        Package("a","b",1L,"v","c",60L,"d"),
+                    ), Binary(
+                        Package("a", "b", 1L, "v", "c", 60L, "d"),
                         60L,
                         time,
                         "d",
@@ -95,8 +83,8 @@ class StatsCalculatorTest {
                         ImageType.jdk,
                         JvmImpl.openj9,
                         Project.jdk
-                    ), Binary (
-                        Package("a","b",1L,"v","c",120L,"d"),
+                    ), Binary(
+                        Package("a", "b", 1L, "v", "c", 120L, "d"),
                         120L,
                         time,
                         "d",
@@ -111,7 +99,7 @@ class StatsCalculatorTest {
                 ),
                 445,
                 Vendor.adoptopenjdk,
-                VersionData(1,2,3,"a",4,5,"c","d")
+                VersionData(1, 2, 3, "a", 4, 5, "c", "d")
             ), Release(
                 "b",
                 ReleaseType.ga,
@@ -121,19 +109,19 @@ class StatsCalculatorTest {
                 time,
                 arrayOf(
                     Binary(
-                        Package("a","b",1L,"v","c",300L,"d"),
-                        300L, //Download count
+                        Package("a", "b", 1L, "v", "c", 300L, "d"),
+                        300L, // Download count
                         time,
                         "d",
-                        Installer("a","b",1L,"v","c",3L),
+                        Installer("a", "b", 1L, "v", "c", 3L),
                         HeapSize.normal,
                         OperatingSystem.linux,
                         Architecture.x64,
                         ImageType.jdk,
                         JvmImpl.hotspot,
                         Project.jdk
-                    ), Binary (
-                        Package("a","b",1L,"v","c",150L,"d"),
+                    ), Binary(
+                        Package("a", "b", 1L, "v", "c", 150L, "d"),
                         150L,
                         time,
                         "d",
@@ -148,9 +136,8 @@ class StatsCalculatorTest {
                 ),
                 450,
                 Vendor.adoptopenjdk,
-                VersionData(1,2,3,"a",4,5,"c","d")
+                VersionData(1, 2, 3, "a", 4, 5, "c", "d")
             )
         )))
     }
-
 }
