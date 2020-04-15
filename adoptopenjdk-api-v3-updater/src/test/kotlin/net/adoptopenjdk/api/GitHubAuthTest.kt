19b1458c7d164b08163c6c2872b4fa5f799a6c8a
John Oliver <1615532+johnoliver@users.noreply.github.com>
2020-04-15 21:47:12 +0100

    more tests/coverage (#172) (#176)
    * more tests/coverage
    
    * Update GitHubAuthTest.kt
    
    * add null check
    
    * clever stuff from John
    
    * Update GitHubAuthTest.kt
    
    * I'm an idiot and I know it
    
    * george isn't such an idiot after all
    
    * add codecov.yml
    
    Co-authored-by: George Adams <georgeadams1995@gmail.com>

---
+++ adoptopenjdk-api-v3-updater/src/test/kotlin/net/adoptopenjdk/api/GitHubAuthTest.kt
@@ -0,0 +1,33 @@
+package net.adoptopenjdk.api
+
+import net.adoptopenjdk.api.v3.dataSources.github.GithubAuth
+import org.junit.jupiter.api.Test
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
+import kotlin.test.assertEquals
+
+class GitHubAuthTest {
+    @Test
+    fun readsTokenFromFile() {
+        System.setProperty("GITHUB_TOKEN", "")
+        System.setProperty("user.home", "/tmp")
+        Files.createDirectory(Path.of("/tmp/.adopt_api"))
+        File("/tmp/.adopt_api/token.properties").printWriter().use { out ->
+            out.println("token=foo")
+        }
+        try {
+            assertEquals("foo", GithubAuth.readToken())
+        } finally {
+            Files.delete(Path.of("/tmp/.adopt_api/token.properties"))
+            Files.delete(Path.of("/tmp/.adopt_api"))
+        }
+    }
+
+    @Test
+    fun readsTokenNullFromFile() {
+        System.setProperty("GITHUB_TOKEN", "")
+        System.setProperty("user.home", "/tmp")
+        assertEquals("", GithubAuth.readToken())
+    }
+}
