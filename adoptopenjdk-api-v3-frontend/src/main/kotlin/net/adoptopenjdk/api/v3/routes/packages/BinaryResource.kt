17802dde6f594e3aa4d5baebc0f099e569167789
John Oliver <1615532+johnoliver@users.noreply.github.com>
2020-04-16 00:55:34 +0000

    Add installer endpoint

--- adoptopenjdk-api-v3-frontend/src/main/kotlin/net/adoptopenjdk/api/v3/routes/packages/BinaryResource.kt
+++ adoptopenjdk-api-v3-frontend/src/main/kotlin/net/adoptopenjdk/api/v3/routes/packages/BinaryResource.kt
@@ -1,17 +1,13 @@
-package net.adoptopenjdk.api.v3.routes
+package net.adoptopenjdk.api.v3.routes.packages
 
-import net.adoptopenjdk.api.v3.JsonMapper
 import net.adoptopenjdk.api.v3.OpenApiDocs
-import net.adoptopenjdk.api.v3.dataSources.APIDataStore
-import net.adoptopenjdk.api.v3.dataSources.SortOrder
-import net.adoptopenjdk.api.v3.filters.BinaryFilter
-import net.adoptopenjdk.api.v3.filters.ReleaseFilter
-import net.adoptopenjdk.api.v3.models.APIError
 import net.adoptopenjdk.api.v3.models.Architecture
+import net.adoptopenjdk.api.v3.models.Binary
 import net.adoptopenjdk.api.v3.models.HeapSize
 import net.adoptopenjdk.api.v3.models.ImageType
 import net.adoptopenjdk.api.v3.models.JvmImpl
 import net.adoptopenjdk.api.v3.models.OperatingSystem
+import net.adoptopenjdk.api.v3.models.Package
 import net.adoptopenjdk.api.v3.models.Project
 import net.adoptopenjdk.api.v3.models.Release
 import net.adoptopenjdk.api.v3.models.ReleaseType
@@ -25,7 +21,6 @@ import org.eclipse.microprofile.openapi.annotations.responses.APIResponses
 import org.eclipse.microprofile.openapi.annotations.tags.Tag
 import org.jboss.resteasy.annotations.jaxrs.PathParam
 import org.jboss.resteasy.annotations.jaxrs.QueryParam
-import java.net.URI
 import javax.ws.rs.GET
 import javax.ws.rs.HEAD
 import javax.ws.rs.HeaderParam
@@ -39,7 +34,7 @@ import javax.ws.rs.core.Response
 @Tag(name = "Binary")
 @Path("/v3/binary/")
 @Produces(MediaType.APPLICATION_JSON)
-class BinaryResource {
+class BinaryResource : PackageEndpoint() {
 
     @GET
     @HEAD
@@ -94,66 +89,27 @@ class BinaryResource {
         @HeaderParam("User-Agent")
         userAgent: String
     ): Response {
+        val releases = getReleases(release_name, vendor, os, arch, image_type, jvm_impl, heap_size, project)
         return when (request.method) {
-            "HEAD" -> versionHead(userAgent, release_name, vendor, os, arch, image_type, jvm_impl, heap_size, project)
-            else -> versionGet(release_name, vendor, os, arch, image_type, jvm_impl, heap_size, project)
-        }
-    }
-
-    private fun versionGet(
-        release_name: String?,
-        vendor: Vendor?,
-        os: OperatingSystem?,
-        arch: Architecture?,
-        image_type: ImageType?,
-        jvm_impl: JvmImpl?,
-        heap_size: HeapSize?,
-        project: Project?
-    ): Response {
-        return getBinary(release_name, vendor, os, arch, image_type, jvm_impl, heap_size, project) { `package` ->
-            Response.temporaryRedirect(URI.create(`package`.link)).build()
+            "HEAD" -> versionHead(userAgent, releases)
+            else -> formResponse(releases)
         }
     }
 
     private fun versionHead(
         userAgent: String,
-        release_name: String?,
-        vendor: Vendor?,
-        os: OperatingSystem?,
-        arch: Architecture?,
-        image_type: ImageType?,
-        jvm_impl: JvmImpl?,
-        heap_size: HeapSize?,
-        project: Project?
+        releases: List<Release>
     ): Response {
         return if (userAgent.contains("Gradle")) {
-            getBinary(release_name, vendor, os, arch, image_type, jvm_impl, heap_size, project) { `package` ->
+            return formResponse(releases) { `package` ->
                 Response.status(200)
                     .header("size", `package`.size)
                     .header("content-disposition", """attachment; filename="${`package`.name}"; filename*=UTF-8''${`package`.name}""")
                     .build()
             }
         } else {
-            versionGet(release_name, vendor, os, arch, image_type, jvm_impl, heap_size, project)
+            formResponse(releases)
         }
-    }
-
-    private fun getBinary(
-        release_name: String?,
-        vendor: Vendor?,
-        os: OperatingSystem?,
-        arch: Architecture?,
-        image_type: ImageType?,
-        jvm_impl: JvmImpl?,
-        heap_size: HeapSize?,
-        project: Project?,
-        createResponse: (net.adoptopenjdk.api.v3.models.Package) -> Response
-    ): Response {
-        val releaseFilter = ReleaseFilter(releaseName = release_name, vendor = vendor)
-        val binaryFilter = BinaryFilter(os, arch, image_type, jvm_impl, heap_size, project)
-        val releases = APIDataStore.getAdoptRepos().getFilteredReleases(releaseFilter, binaryFilter, SortOrder.DESC).toList()
-
-        return formResponse(releases, createResponse)
     }
 
     @GET
@@ -205,53 +161,21 @@ class BinaryResource {
         @QueryParam("project")
         project: Project?
     ): Response {
-        val releaseFilter = ReleaseFilter(releaseType = release_type, featureVersion = version, vendor = vendor)
-        val binaryFilter = BinaryFilter(os, arch, image_type, jvm_impl, heap_size, project)
-        val releases = APIDataStore.getAdoptRepos().getFilteredReleases(releaseFilter, binaryFilter, SortOrder.DESC).toList()
+        val releaseList = getRelease(release_type, version, vendor, os, arch, image_type, jvm_impl, heap_size, project)
 
-        val comparator = compareBy<Release> { it.version_data.major }
-            .thenBy { it.version_data.minor }
-            .thenBy { it.version_data.security }
-            .thenBy { it.version_data.pre }
-            .thenBy { it.version_data.build }
-            .thenBy { it.version_data.adopt_build_number }
-            .thenBy { it.version_data.optional }
+        val release = releaseList.lastOrNull()
 
-        val release = releases.sortedWith(comparator).lastOrNull()
-
-        return formResponse(if (release == null) emptyList() else listOf(release)) { `package` ->
-            Response.temporaryRedirect(URI.create(`package`.link)).build()
-        }
+        return formResponse(if (release == null) emptyList() else listOf(release))
     }
 
-    private fun formResponse(releases: List<Release>, createResponse: (net.adoptopenjdk.api.v3.models.Package) -> Response): Response {
-        if (releases.size == 0) {
-            return formErrorResponse(Response.Status.NOT_FOUND, "No releases match the request")
-        } else if (releases.size > 1) {
-            val versions = releases
-                .map { it.release_name }
-            return formErrorResponse(Response.Status.BAD_REQUEST, "Multiple releases match request: $versions")
-        } else {
-            val binaries = releases.get(0).binaries
-            val packages = binaries
-                .map { it.`package` }
-                .filterNotNull()
-
-            if (packages.size == 0) {
-                return formErrorResponse(Response.Status.NOT_FOUND, "No binaries match the request")
-            } else if (packages.size > 1) {
-                val names = packages.map { it.name }
-                return formErrorResponse(Response.Status.BAD_REQUEST, "Multiple binaries match request: $names")
-            } else {
-                return createResponse(packages.first())
-            }
-        }
+    protected fun formResponse(
+        releases: List<Release>,
+        createResponse: (Package) -> Response = redirectToAsset()
+    ): Response {
+        return formResponse(releases, extractPackage(), createResponse)
     }
 
-    private fun formErrorResponse(status: Response.Status, message: String): Response {
-        return Response
-            .status(status)
-            .entity(JsonMapper.mapper.writeValueAsString(APIError(message)))
-            .build()
+    private fun extractPackage(): (binary: Binary) -> Package {
+        return { binary -> binary.`package` }
     }
 }
--- adoptopenjdk-api-v3-frontend/src/test/kotlin/net/adoptopenjdk/api/packages/BinaryPathTest.kt
+++ adoptopenjdk-api-v3-frontend/src/test/kotlin/net/adoptopenjdk/api/packages/BinaryPathTest.kt
@@ -1,12 +1,7 @@
-package net.adoptopenjdk.api
+package net.adoptopenjdk.api.packages
 
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured
-import io.restassured.response.Response
-import kotlinx.coroutines.runBlocking
-import net.adoptopenjdk.api.v3.AdoptReposBuilder
-import net.adoptopenjdk.api.v3.dataSources.APIDataStore
-import net.adoptopenjdk.api.v3.dataSources.ApiPersistenceFactory
 import net.adoptopenjdk.api.v3.models.Architecture
 import net.adoptopenjdk.api.v3.models.HeapSize
 import net.adoptopenjdk.api.v3.models.ImageType
@@ -16,104 +11,64 @@ import net.adoptopenjdk.api.v3.models.Project
 import net.adoptopenjdk.api.v3.models.ReleaseType
 import net.adoptopenjdk.api.v3.models.Vendor
 import org.hamcrest.Matchers
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 
 @QuarkusTest
-class BinaryPathTest : BaseTest() {
+class BinaryPathTest : PackageEndpointTest() {
 
-    companion object {
-        @JvmStatic
-        @BeforeAll
-        fun populateDb() {
-            runBlocking {
-                val repo = AdoptReposBuilder.build(APIDataStore.variants.versions)
-                // Reset connection
-                ApiPersistenceFactory.set(null)
-                ApiPersistenceFactory.get().updateAllRepos(repo)
-                APIDataStore.loadDataFromDb()
-            }
-        }
-    }
-
-    val path = "/v3/binary"
-
-    fun getLatestPath(
-        featureVersion: Int,
-        releaseType: ReleaseType,
-        os: OperatingSystem,
-        arch: Architecture,
-        imageType: ImageType,
-        jvmImpl: JvmImpl,
-        heapSize: HeapSize,
-        vendor: Vendor,
-        project: Project
-    ): String {
-        return "$path/latest/$featureVersion/$releaseType/$os/$arch/$imageType/$jvmImpl/$heapSize/$vendor?project=$project"
-    }
-
-    fun getVersionPath(
-        releaseName: String,
-        os: OperatingSystem,
-        arch: Architecture,
-        imageType: ImageType,
-        jvmImpl: JvmImpl,
-        heapSize: HeapSize,
-        vendor: Vendor,
-        project: Project
-    ): String {
-        return "$path/version/$releaseName/$os/$arch/$imageType/$jvmImpl/$heapSize/$vendor?project=$project"
+    override fun getPath(): String {
+        return "/v3/binary"
     }
 
     @Test
     fun latestDoesRedirectToBinary() {
         val path = getLatestPath(8, ReleaseType.ga, OperatingSystem.linux, Architecture.x64, ImageType.jdk, JvmImpl.hotspot, HeapSize.normal, Vendor.adoptopenjdk, Project.jdk)
         performRequest(path)
-                .then()
-                .statusCode(307)
-                .header("Location", Matchers.startsWith("https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/"))
+            .then()
+            .statusCode(307)
+            .header("Location", Matchers.startsWith("https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/"))
     }
 
     @Test
     fun latestDoesRedirectToBinaryNoProject() {
-        val path = "$path/latest/11/ga/linux/x64/jdk/openj9/normal/adoptopenjdk"
+        val path = "${getPath()}/latest/11/ga/linux/x64/jdk/openj9/normal/adoptopenjdk"
         performRequest(path)
-                .then()
-                .statusCode(307)
-                .header("Location", Matchers.startsWith("https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/"))
+            .then()
+            .statusCode(307)
+            .header("Location", Matchers.startsWith("https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/"))
     }
 
     @Test
     fun latestDoesNotRedirectToBinary() {
         val path = getLatestPath(8, ReleaseType.ga, OperatingSystem.linux, Architecture.x64, ImageType.jdk, JvmImpl.hotspot, HeapSize.normal, Vendor.adoptopenjdk, Project.valhalla)
         performRequest(path)
-                .then()
-                .statusCode(404)
+            .then()
+            .statusCode(404)
     }
 
     @Test
-    fun noExistantLatestRequestGives404() {
+    fun nonExistantLatestRequestGives404() {
         val path = getLatestPath(4, ReleaseType.ga, OperatingSystem.linux, Architecture.x64, ImageType.jdk, JvmImpl.hotspot, HeapSize.normal, Vendor.adoptopenjdk, Project.valhalla)
         performRequest(path)
-                .then()
-                .statusCode(404)
+            .then()
+            .statusCode(404)
     }
 
     @Test
-    fun nonExistantVersionRequestRedirects() {
+    fun versionRequestRedirects() {
         val path = getVersionPath("jdk8u212-b04", OperatingSystem.linux, Architecture.x64, ImageType.jdk, JvmImpl.hotspot, HeapSize.normal, Vendor.adoptopenjdk, Project.jdk)
         performRequest(path)
-                .then()
-                .statusCode(307)
-                .header("Location", Matchers.startsWith("https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u212-b04"))
+            .then()
+            .statusCode(307)
+            .header("Location", Matchers.startsWith("https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u212-b04"))
     }
 
     @Test
     fun nonExistantVersionRequestGives404() {
         val path = getVersionPath("fooBar", OperatingSystem.linux, Architecture.x64, ImageType.jdk, JvmImpl.hotspot, HeapSize.normal, Vendor.adoptopenjdk, Project.jdk)
         performRequest(path)
-                .then()
-                .statusCode(404)
+            .then()
+            .statusCode(404)
     }
 
     @Test
@@ -152,12 +107,5 @@ class BinaryPathTest : BaseTest() {
             .head(path)
             .then()
             .statusCode(404)
-    }
-
-    private fun performRequest(path: String): Response {
-        return RestAssured.given()
-                .`when`()
-                .redirects().follow(false)
-                .get(path)
     }
 }
