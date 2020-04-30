9c0579f332df1158a628461a9418dcd572bbb55b
John Oliver <1615532+johnoliver@users.noreply.github.com>
2020-04-30 13:43:40 +0000

    fix displaying correct value in swagger-ui

--- adoptopenjdk-api-v3-models/src/main/kotlin/net/adoptopenjdk/api/v3/models/ImageType.kt
+++ adoptopenjdk-api-v3-models/src/main/kotlin/net/adoptopenjdk/api/v3/models/ImageType.kt
@@ -3,7 +3,7 @@ package net.adoptopenjdk.api.v3.models
 import org.eclipse.microprofile.openapi.annotations.enums.SchemaType
 import org.eclipse.microprofile.openapi.annotations.media.Schema
 
-@Schema(type = SchemaType.STRING, enumeration = ["jdk", "jre", "testimage", "debugimage", "static-libs"], example = "jdk")
+@Schema(type = SchemaType.STRING, enumeration = ["jdk", "jre", "testimage", "debugimage", "staticlibs"], example = "jdk")
 enum class ImageType : FileNameMatcher {
     jdk, jre, testimage, debugimage, staticlibs("static-libs");
 
