0f40e609a16d1822546785e35d49c228ff8c385c
John Oliver <1615532+johnoliver@users.noreply.github.com>
2020-06-02 11:27:38 +0000

    Fix #217

--- adoptopenjdk-api-v3-frontend/src/test/kotlin/net/adoptopenjdk/api/AvailableReleasesPathTest.kt
+++ adoptopenjdk-api-v3-frontend/src/test/kotlin/net/adoptopenjdk/api/AvailableReleasesPathTest.kt
@@ -2,17 +2,101 @@ package net.adoptopenjdk.api
 
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured
+import kotlinx.coroutines.runBlocking
+import net.adoptopenjdk.api.v3.AdoptReposBuilder
+import net.adoptopenjdk.api.v3.JsonMapper
+import net.adoptopenjdk.api.v3.dataSources.APIDataStore
+import net.adoptopenjdk.api.v3.dataSources.ApiPersistenceFactory
+import net.adoptopenjdk.api.v3.dataSources.models.AdoptRepos
+import net.adoptopenjdk.api.v3.dataSources.models.FeatureRelease
+import net.adoptopenjdk.api.v3.dataSources.models.Releases
+import net.adoptopenjdk.api.v3.models.ReleaseInfo
+import net.adoptopenjdk.api.v3.models.ReleaseType
+import org.hamcrest.Description
+import org.hamcrest.TypeSafeMatcher
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 @QuarkusTest
 class AvailableReleasesPathTest : BaseTest() {
+
+    @BeforeEach
+    fun initDB() {
+        runBlocking {
+            val repo = AdoptReposBuilder.build(APIDataStore.variants.versions)
+
+            val releases = repo.allReleases.getReleases()
+                .filter { it.version_data.major < 13 || it.version_data.major == 13 && it.release_type == ReleaseType.ea }
+                .groupBy { it.version_data.major }
+                .toMap()
+                .map { FeatureRelease(it.key, Releases(it.value)) }
+
+            // Reset connection
+            ApiPersistenceFactory.set(null)
+            ApiPersistenceFactory.get().updateAllRepos(AdoptRepos(releases))
+            APIDataStore.loadDataFromDb()
+        }
+    }
+
     @Test
     fun availableReleases() {
-
         RestAssured.given()
-                .`when`()
-                .get("/v3/info/available_releases")
-                .then()
-                .statusCode(200)
+            .`when`()
+            .get("/v3/info/available_releases")
+            .then()
+            .statusCode(200)
+    }
+
+    @Test
+    fun availableVersionsIsCorrect() {
+        check { releaseInfo ->
+            releaseInfo.available_releases.contentEquals(arrayOf(8, 9, 10, 11, 12))
+        }
+    }
+
+    @Test
+    fun availableLtsIsCorrect() {
+        check { releaseInfo ->
+            releaseInfo.available_lts_releases.contentEquals(arrayOf(8, 11))
+        }
+    }
+
+    @Test
+    fun mostRecentLtsIsCorrect() {
+        check { releaseInfo ->
+            releaseInfo.most_recent_lts == 11
+        }
+    }
+
+    @Test
+    fun mostRecentFeatureReleaseIsCorrect() {
+        check { releaseInfo ->
+            releaseInfo.most_recent_feature_release == 12
+        }
+    }
+
+    @Test
+    fun mostRecentFeatureVersionIsCorrect() {
+        check { releaseInfo ->
+            releaseInfo.most_recent_feature_version == 13
+        }
+    }
+
+    private fun check(matcher: (ReleaseInfo) -> Boolean) {
+        RestAssured.given()
+            .`when`()
+            .get("/v3/info/available_releases")
+            .then()
+            .body(object : TypeSafeMatcher<String>() {
+
+                override fun describeTo(description: Description?) {
+                    description!!.appendText("json")
+                }
+
+                override fun matchesSafely(p0: String?): Boolean {
+                    val releaseInfo = JsonMapper.mapper.readValue(p0, ReleaseInfo::class.java)
+                    return matcher(releaseInfo)
+                }
+            })
     }
 }
--- adoptopenjdk-api-v3-models/src/main/kotlin/net/adoptopenjdk/api/v3/models/ReleaseInfo.kt
+++ adoptopenjdk-api-v3-models/src/main/kotlin/net/adoptopenjdk/api/v3/models/ReleaseInfo.kt
@@ -4,27 +4,32 @@ import org.eclipse.microprofile.openapi.annotations.media.Schema
 
 class ReleaseInfo {
 
-    @Schema(example = "[8,9,10,11,12,13,14]")
+    @Schema(example = "[8,9,10,11,12,13,14]", description = "The versions for which adopt have produced a ga release")
     val available_releases: Array<Int>
 
-    @Schema(example = "[8,11]")
+    @Schema(example = "[8,11]", description = "The LTS versions for which adopt have produced a ga release")
     val available_lts_releases: Array<Int>
 
-    @Schema(example = "11")
+    @Schema(example = "11", description = "The highest LTS version for which adopt have produced a ga release")
     val most_recent_lts: Int
 
-    @Schema(example = "13")
+    @Schema(example = "13", description = "The highest version (LTS or not) for which adopt have produced a ga release")
     val most_recent_feature_release: Int
+
+    @Schema(example = "15", description = "The highest version (LTS or not) for which we have produced a build, this may be a version that has not yet produced a ga release")
+    val most_recent_feature_version: Int
 
     constructor(
         available_releases: Array<Int>,
         available_lts_releases: Array<Int>,
         most_recent_lts: Int,
-        most_recent_feature_release: Int
+        most_recent_feature_release: Int,
+        most_recent_feature_version: Int
     ) {
         this.available_releases = available_releases
         this.available_lts_releases = available_lts_releases
         this.most_recent_lts = most_recent_lts
         this.most_recent_feature_release = most_recent_feature_release
+        this.most_recent_feature_version = most_recent_feature_version
     }
 }
