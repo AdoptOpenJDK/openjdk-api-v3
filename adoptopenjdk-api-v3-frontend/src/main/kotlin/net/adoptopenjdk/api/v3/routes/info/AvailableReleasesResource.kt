7b0dfc212d902c33284684f966662d54cd9fa6c5
John Oliver <1615532+johnoliver@users.noreply.github.com>
2020-06-05 15:44:38 +0100

    Fix #217 (#218)

--- adoptopenjdk-api-v3-frontend/src/main/kotlin/net/adoptopenjdk/api/v3/routes/info/AvailableReleasesResource.kt
+++ adoptopenjdk-api-v3-frontend/src/main/kotlin/net/adoptopenjdk/api/v3/routes/info/AvailableReleasesResource.kt
@@ -2,6 +2,7 @@ package net.adoptopenjdk.api.v3.routes.info
 
 import net.adoptopenjdk.api.v3.dataSources.APIDataStore
 import net.adoptopenjdk.api.v3.models.ReleaseInfo
+import net.adoptopenjdk.api.v3.models.ReleaseType
 import org.eclipse.microprofile.openapi.annotations.Operation
 import org.eclipse.microprofile.openapi.annotations.tags.Tag
 import javax.ws.rs.GET
@@ -14,15 +15,53 @@ import javax.ws.rs.core.MediaType
 @Produces(MediaType.APPLICATION_JSON)
 class AvailableReleasesResource {
 
+    private var releaseInfo: ReleaseInfo = formReleaseInfo()
+
     @GET
     @Path("/available_releases/")
     @Operation(summary = "Returns information about available releases")
     fun get(): ReleaseInfo {
+        return releaseInfo
+    }
+
+    final fun formReleaseInfo(): ReleaseInfo {
+        val gaReleases = APIDataStore.getAdoptRepos()
+            .allReleases
+            .getReleases()
+            .filter { it.release_type == ReleaseType.ga }
+            .toList()
+
+        val availableReleases = gaReleases
+            .map { it.version_data.major }
+            .distinct()
+            .sorted()
+            .toList()
+            .toTypedArray()
+        val mostRecentFeatureRelease: Int = availableReleases.last()
+
+        val availableLtsReleases: Array<Int> = gaReleases
+            .filter { APIDataStore.variants.ltsVersions.contains(it.version_data.major) }
+            .map { it.version_data.major }
+            .distinct()
+            .sorted()
+            .toList()
+            .toTypedArray()
+        val mostRecentLts = availableLtsReleases.last()
+
+        val mostRecentFeatureVersion: Int = APIDataStore.getAdoptRepos()
+            .allReleases
+            .getReleases()
+            .map { it.version_data.major }
+            .distinct()
+            .sorted()
+            .last()
+
         return ReleaseInfo(
-            APIDataStore.variants.versions,
-            APIDataStore.variants.ltsVersions,
-            APIDataStore.variants.latestLtsVersion,
-            APIDataStore.variants.latestVersion
+            availableReleases,
+            availableLtsReleases,
+            mostRecentLts,
+            mostRecentFeatureRelease,
+            mostRecentFeatureVersion
         )
     }
 }
