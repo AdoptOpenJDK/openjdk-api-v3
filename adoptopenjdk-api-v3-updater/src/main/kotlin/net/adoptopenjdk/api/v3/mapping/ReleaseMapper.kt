87324f4de3119e42cfed65f7cde6f4b364eb9f37
John Oliver <1615532+johnoliver@users.noreply.github.com>
2020-06-16 23:29:35 +0000

    Merge master

--- adoptopenjdk-api-v3-updater/src/main/kotlin/net/adoptopenjdk/api/v3/mapping/ReleaseMapper.kt
+++ adoptopenjdk-api-v3-updater/src/main/kotlin/net/adoptopenjdk/api/v3/mapping/ReleaseMapper.kt
@@ -1,19 +1,19 @@
 package net.adoptopenjdk.api.v3.mapping
 
+import net.adoptopenjdk.api.v3.ReleaseResult
 import net.adoptopenjdk.api.v3.TimeSource
 import net.adoptopenjdk.api.v3.dataSources.github.graphql.models.GHRelease
-import net.adoptopenjdk.api.v3.models.Release
 import java.time.Instant
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
 abstract class ReleaseMapper {
-    abstract suspend fun toAdoptRelease(release: GHRelease): List<Release>?
+    abstract suspend fun toAdoptRelease(release: GHRelease): ReleaseResult
 
     companion object {
         fun parseDate(date: String): ZonedDateTime {
             return Instant.from(DateTimeFormatter.ISO_INSTANT.parse(date))
-                    .atZone(TimeSource.ZONE)
+                .atZone(TimeSource.ZONE)
         }
     }
 }
