7a86c1f4324c997ea93bab88478e2637d8a3f4c5
John Oliver <1615532+johnoliver@users.noreply.github.com>
2020-07-23 13:01:09 +0100

    add application insights (#249)

--- adoptopenjdk-api-v3-frontend/src/main/kotlin/net/adoptopenjdk/api/v3/routes/info/PlatformsResource.kt
+++ adoptopenjdk-api-v3-frontend/src/main/kotlin/net/adoptopenjdk/api/v3/routes/info/PlatformsResource.kt
@@ -2,6 +2,7 @@ package net.adoptopenjdk.api.v3.routes.info
 
 import net.adoptopenjdk.api.v3.dataSources.APIDataStore
 import net.adoptopenjdk.api.v3.models.Platforms
+import org.eclipse.microprofile.metrics.annotation.Timed
 import org.eclipse.microprofile.openapi.annotations.Operation
 import org.eclipse.microprofile.openapi.annotations.tags.Tag
 import javax.ws.rs.GET
@@ -12,6 +13,7 @@ import javax.ws.rs.core.MediaType
 @Tag(name = "Release Info")
 @Path("/v3/info/")
 @Produces(MediaType.APPLICATION_JSON)
+@Timed
 class PlatformsResource {
 
     @GET
