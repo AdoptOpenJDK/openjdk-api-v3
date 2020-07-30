f74cd6eec34b020dda69229e28ed0b7316fdc52a
John Oliver <1615532+johnoliver@users.noreply.github.com>
2020-07-31 00:30:14 +0100

    Make UpdatedInfo a data class (#265)

--- adoptopenjdk-api-v3-persistance/src/main/kotlin/net/adoptopenjdk/api/v3/dataSources/persitence/mongo/UpdatedInfo.kt
+++ adoptopenjdk-api-v3-persistance/src/main/kotlin/net/adoptopenjdk/api/v3/dataSources/persitence/mongo/UpdatedInfo.kt
@@ -2,7 +2,7 @@ package net.adoptopenjdk.api.v3.dataSources.persitence.mongo
 
 import java.time.ZonedDateTime
 
-class UpdatedInfo(val time: ZonedDateTime, val checksum: String) {
+data class UpdatedInfo(val time: ZonedDateTime, val checksum: String) {
     override fun toString(): String {
         return "$time $checksum"
     }
