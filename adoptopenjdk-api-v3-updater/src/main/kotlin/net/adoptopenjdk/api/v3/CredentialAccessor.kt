ab1b8b154759f78a0c15db9a0f6bd60642e9b1a0
Parker Mauney <parkermauney@gmail.com>
2020-07-30 12:43:24 -0500

    Some docs + small refactor in getPropertyFromFile

--- adoptopenjdk-api-v3-updater/src/main/kotlin/net/adoptopenjdk/api/v3/CredentialAccessor.kt
+++ adoptopenjdk-api-v3-updater/src/main/kotlin/net/adoptopenjdk/api/v3/CredentialAccessor.kt
@@ -1,7 +1,7 @@
 package net.adoptopenjdk.api.v3
 
-import java.io.File
 import java.nio.file.Files
+import java.nio.file.Path
 import java.util.Properties
 
 interface CredentialAccessor {
@@ -15,13 +15,34 @@ class SystemCredentialAccessor : CredentialAccessor {
     override fun getProperty(key: String): String? = System.getProperty(key) ?: null
 
     /**
-     * Searches a file ~/.adopt_api/<filename> for a property named <key>
+     * Searches a file ~/.adopt_api/[filename] for a property named [key].
+     *
+     *  For example:
+     *
+     *  * Given the following user system configuration:
+     *      ```
+     *      $ ls -d ~/.adopt_api/somefile.properties
+     *      /home/foo/.adopt_api/somefile.properties
+     *      $ cat /home/foo/.adopt_api/somefile.properties
+     *      my.local.property=p455w3rd
+     *      ```
+     *
+     *  * Then, the following assertion should hold
+     *      ```
+     *      val creds = getPropertyFromFile(
+     *          key = "my.local.property",
+     *          filename = "somefile.properties"
+     *      )
+     *      assert(creds == "p455w3rd")
+     *      ```
+     *
+     * @param key Property name to look for
+     * @param filename Name of property file located at `~/.adopt_api`
+     * @return `some-value` where `~/.adopt_api`/[filename] contains the line [key]`=some-value`
      */
     override fun getPropertyFromFile(key: String, filename: String): String? {
         val userHome = System.getProperty("user.home")
-
-        // e.g /home/foo/.adopt_api/token.properties
-        val propertiesFile = File(userHome + File.separator + ".adopt_api" + File.separator + filename)
+        val propertiesFile = Path.of(userHome, ".adopt_api", filename).toFile()
 
         var token = ""
         if (propertiesFile.exists()) {
