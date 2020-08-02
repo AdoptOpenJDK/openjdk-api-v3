6cbe95ea47e62be085f479dda2c819b1ab42d006
Parker Mauney <parkermauney@gmail.com>
2020-08-02 00:31:07 -0500

    Disable GitHubAuthTest if an env var or system property token exists
     + Restore any modified properties/clean up created test files
     + Rename GithubAuth -> GitHubAuth

--- adoptopenjdk-api-v3-updater/src/test/kotlin/net/adoptopenjdk/api/GitHubAuthTest.kt
+++ adoptopenjdk-api-v3-updater/src/test/kotlin/net/adoptopenjdk/api/GitHubAuthTest.kt
@@ -1,33 +1,99 @@
 package net.adoptopenjdk.api
 
-import net.adoptopenjdk.api.v3.dataSources.github.GithubAuth
+import net.adoptopenjdk.api.v3.dataSources.github.GitHubAuth
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.oneOf
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty
 import java.io.File
 import java.nio.file.Files
-import java.nio.file.Path
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 
+/**
+ * GitHubAuth makes actual calls to System.getenv and System.getProperty, so ignore these tests if an
+ * env var or property named GITHUB_TOKEN exists to prevent real tokens being inadvertently logged as assertion errors.
+ */
+@DisabledIfEnvironmentVariable(named = GitHubAuthTest.tokenKey, matches = ".*")
+@DisabledIfSystemProperty(named = GitHubAuthTest.tokenKey, matches = ".*")
 class GitHubAuthTest {
-    @Test
-    fun readsTokenFromFile() {
-        System.setProperty("GITHUB_TOKEN", "")
-        System.setProperty("user.home", "/tmp")
-        Files.createDirectory(Path.of("/tmp/.adopt_api"))
-        File("/tmp/.adopt_api/token.properties").printWriter().use { out ->
-            out.println("token=foo")
+
+    companion object {
+        const val tokenKey = "GITHUB_TOKEN"
+        private lateinit var tempDir: File
+        private lateinit var tokenFile: File
+        private lateinit var savedUserHome: String
+
+        @JvmStatic @BeforeAll
+        fun setUpClass() {
+            tempDir = Files.createTempDirectory("testUserHome").toFile()
+
+            savedUserHome = System.getProperty("user.home")
+            System.setProperty("user.home", tempDir.absolutePath)
         }
+
+        @JvmStatic @AfterAll
+        fun cleanUp() {
+            tempDir.deleteRecursively()
+            System.setProperty("user.home", savedUserHome)
+        }
+    }
+
+    @BeforeEach
+    fun setUp() {
+        assertFalse(System.getenv().containsKey(tokenKey))
+        assertFalse(System.getProperties().containsKey(tokenKey))
+    }
+
+    @Test
+    fun `readToken prioritizes system property if env var is not defined`() {
+        assertFalse(System.getenv().containsKey(tokenKey))
+
+        val prevTokenProperty: String? = System.getProperty(tokenKey)
+        System.setProperty(tokenKey, "system-property-token")
+
         try {
-            assertEquals("foo", GithubAuth.readToken())
+            val actualToken = GitHubAuth.readToken()
+            assertEquals("system-property-token", actualToken)
         } finally {
-            Files.delete(Path.of("/tmp/.adopt_api/token.properties"))
-            Files.delete(Path.of("/tmp/.adopt_api"))
+            if (prevTokenProperty == null) {
+                System.clearProperty(tokenKey)
+            } else {
+                System.setProperty(tokenKey, prevTokenProperty)
+            }
+        }
+    }
+
+    @Test
+    fun `readToken falls back to property file if env var and system property are not defined`() {
+        assertFalse(System.getenv().containsKey(tokenKey))
+        assertFalse(System.getProperties().containsKey(tokenKey))
+
+        val tokenDir = File(tempDir, ".adopt_api")
+        tokenDir.mkdirs()
+        tokenFile = File(tokenDir, "token.properties")
+        tokenFile.printWriter().use { out ->
+            out.println("token=real-file-token")
+        }
+
+        try {
+            val actualToken = GitHubAuth.readToken()
+            assertEquals("real-file-token", actualToken)
+        } finally {
+            tokenDir.deleteRecursively()
         }
     }
 
     @Test
     fun readsTokenNullFromFile() {
-        System.setProperty("GITHUB_TOKEN", "")
-        System.setProperty("user.home", "/tmp")
-        assertEquals("", GithubAuth.readToken())
+        assertFalse(System.getenv().containsKey(tokenKey))
+        assertFalse(File(tempDir, ".adopt_api").exists())
+
+        val actualToken = GitHubAuth.readToken()
+        assertThat(actualToken, oneOf(null, ""))
     }
 }
