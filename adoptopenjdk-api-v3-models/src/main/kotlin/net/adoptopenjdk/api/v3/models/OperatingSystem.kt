68d0aeb4fef442b78e6e81ba0ffdf3079efb2e41
John Oliver <1615532+johnoliver@users.noreply.github.com>
2020-11-25 12:43:30 +0000

    Make alpine-linux a seperate os

--- adoptopenjdk-api-v3-models/src/main/kotlin/net/adoptopenjdk/api/v3/models/OperatingSystem.kt
+++ adoptopenjdk-api-v3-models/src/main/kotlin/net/adoptopenjdk/api/v3/models/OperatingSystem.kt
@@ -3,13 +3,14 @@ package net.adoptopenjdk.api.v3.models
 import org.eclipse.microprofile.openapi.annotations.enums.SchemaType
 import org.eclipse.microprofile.openapi.annotations.media.Schema
 
-@Schema(type = SchemaType.STRING, enumeration = ["linux", "windows", "mac", "solaris", "aix"])
+@Schema(type = SchemaType.STRING, enumeration = ["linux", "windows", "mac", "solaris", "aix", "alpine-linux"])
 enum class OperatingSystem : FileNameMatcher {
-    linux(0, "LinuxLH", "alpine-linux"),
+    linux(0, "LinuxLH"),
     windows(0, "win"),
     mac,
     solaris,
-    aix;
+    aix,
+    `alpine-linux`;
 
     override lateinit var names: List<String>
     override var priority: Int = 0
