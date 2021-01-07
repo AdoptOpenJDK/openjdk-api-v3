025805eb992bca83b87759ae6fb2893030321c01
Aleksei Arsenev <alesharikReserv@yandex.ru>
2021-01-07 22:45:44 +0300

    Split /version/{release_name}/{os}/{arch}/{image_type}/{jvm_impl}/{heap_size}/{vendor} method in two to have different operationIds

--- adoptopenjdk-api-v3-frontend/src/main/kotlin/net/adoptopenjdk/api/v3/routes/packages/BinaryResource.kt
+++ adoptopenjdk-api-v3-frontend/src/main/kotlin/net/adoptopenjdk/api/v3/routes/packages/BinaryResource.kt
@@ -28,9 +28,7 @@ import javax.ws.rs.HEAD
 import javax.ws.rs.HeaderParam
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
-import javax.ws.rs.core.Context
 import javax.ws.rs.core.MediaType
-import javax.ws.rs.core.Request
 import javax.ws.rs.core.Response
 
 @Tag(name = "Binary")
@@ -40,7 +38,6 @@ import javax.ws.rs.core.Response
 class BinaryResource @Inject constructor(private val packageEndpoint: PackageEndpoint) {
 
     @GET
-    @HEAD
     @Path("/version/{release_name}/{os}/{arch}/{image_type}/{jvm_impl}/{heap_size}/{vendor}")
     @Produces("application/octet-stream")
     @Operation(
@@ -89,26 +86,68 @@ class BinaryResource @Inject constructor(private val packageEndpoint: PackageEnd
 
         @Parameter(name = "project", description = "Project", required = false)
         @QueryParam("project")
-        project: Project?,
+        project: Project?
+    ): Response {
+        val releases = packageEndpoint.getReleases(release_name, vendor, os, arch, image_type, jvm_impl, heap_size, project)
+        return formResponse(releases)
+    }
 
-        @Context
-        request: Request,
+    @HEAD
+    @Path("/version/{release_name}/{os}/{arch}/{image_type}/{jvm_impl}/{heap_size}/{vendor}")
+    @Produces("application/octet-stream")
+    @Operation(
+        operationId = "getBinaryByVersionHead",
+        summary = "Redirects to the binary that matches your current query",
+        description = "Redirects to the binary that matches your current query"
+    )
+    @APIResponses(
+        value = [
+            APIResponse(responseCode = "307", description = "link to binary that matches your current query"),
+            APIResponse(responseCode = "400", description = "bad input parameter"),
+            APIResponse(responseCode = "404", description = "No matching binary found")
+        ]
+    )
+    fun returnBinaryByVersionHead(
+        @Parameter(name = "os", description = "Operating System", required = true)
+        @PathParam("os")
+        os: OperatingSystem?,
+
+        @Parameter(name = "arch", description = "Architecture", required = true)
+        @PathParam("arch")
+        arch: Architecture?,
+
+        @Parameter(
+            name = "release_name", description = OpenApiDocs.RELASE_NAME, required = true,
+            schema = Schema(defaultValue = "jdk-11.0.6+10", type = SchemaType.STRING)
+        )
+        @PathParam("release_name")
+        release_name: String?,
+
+        @Parameter(name = "image_type", description = "Image Type", required = true)
+        @PathParam("image_type")
+        image_type: ImageType?,
+
+        @Parameter(name = "jvm_impl", description = "JVM Implementation", required = true)
+        @PathParam("jvm_impl")
+        jvm_impl: JvmImpl?,
+
+        @Parameter(name = "heap_size", description = "Heap Size", required = true)
+        @PathParam("heap_size")
+        heap_size: HeapSize?,
+
+        @Parameter(name = "vendor", description = OpenApiDocs.VENDOR, required = true)
+        @PathParam("vendor")
+        vendor: Vendor?,
+
+        @Parameter(name = "project", description = "Project", required = false)
+        @QueryParam("project")
+        project: Project?,
 
         @Parameter(hidden = true, required = false)
         @HeaderParam("User-Agent")
         userAgent: String
     ): Response {
         val releases = packageEndpoint.getReleases(release_name, vendor, os, arch, image_type, jvm_impl, heap_size, project)
-        return when (request.method) {
-            "HEAD" -> versionHead(userAgent, releases)
-            else -> formResponse(releases)
-        }
-    }
-
-    private fun versionHead(
-        userAgent: String,
-        releases: List<Release>
-    ): Response {
         return if (userAgent.contains("Gradle")) {
             return formResponse(releases) { `package` ->
                 Response.status(200)
