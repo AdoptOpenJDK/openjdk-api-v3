baee4a5f471527f233cf648183ad9e689a2654e2
Debjit Mandal <debjitmandal@microsoft.com>
2021-05-10 17:33:59 -0700

    PR comments addressed

--- adoptopenjdk-api-v3-frontend/src/main/kotlin/net/adoptopenjdk/api/v3/Pagination.kt
+++ adoptopenjdk-api-v3-frontend/src/main/kotlin/net/adoptopenjdk/api/v3/Pagination.kt
@@ -6,10 +6,13 @@ import kotlin.math.min
 object Pagination {
     private const val defaultPageSizeNum = 10
     private const val maxPageSizeNum = 20
+    const val largerPageSizeNum = 50
     const val defaultPageSize = defaultPageSizeNum.toString()
     const val maxPageSize = maxPageSizeNum.toString()
+    const val largerPageSize = largerPageSizeNum.toString()
 
-    fun <T> getPage(pageSize: Int?, page: Int?, releases: Sequence<T>, maxPageSizeNum: Int = 20): List<T> {
+
+    fun <T> getPage(pageSize: Int?, page: Int?, releases: Sequence<T>, maxPageSizeNum: Int = this.maxPageSizeNum): List<T> {
         val pageSizeNum = min(maxPageSizeNum, (pageSize ?: defaultPageSizeNum))
         val pageNum = page ?: 0
 
