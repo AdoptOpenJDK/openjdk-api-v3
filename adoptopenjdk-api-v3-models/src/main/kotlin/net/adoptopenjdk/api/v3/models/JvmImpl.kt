778c48c83d24c095a2b9e0533200b090c8580fab
Debjit Mandal <debjitmandal@microsoft.com>
2021-05-26 12:23:40 -0700

    new vendor and jvmimpl order changed

--- adoptopenjdk-api-v3-models/src/main/kotlin/net/adoptopenjdk/api/v3/models/JvmImpl.kt
+++ adoptopenjdk-api-v3-models/src/main/kotlin/net/adoptopenjdk/api/v3/models/JvmImpl.kt
@@ -3,9 +3,9 @@ package net.adoptopenjdk.api.v3.models
 import org.eclipse.microprofile.openapi.annotations.enums.SchemaType
 import org.eclipse.microprofile.openapi.annotations.media.Schema
 
-@Schema(type = SchemaType.STRING, enumeration = ["dragonwell", "hotspot", "openj9"])
+@Schema(type = SchemaType.STRING, enumeration = ["dragonwell", "openj9", "hotspot"])
 enum class JvmImpl : FileNameMatcher {
-    dragonwell, hotspot, openj9;
+    dragonwell, openj9, hotspot;
 
     override lateinit var names: List<String>
     override var priority: Int = 0
