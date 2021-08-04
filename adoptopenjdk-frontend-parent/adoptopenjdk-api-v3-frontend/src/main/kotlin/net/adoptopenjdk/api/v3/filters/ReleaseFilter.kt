c7e022a509cbb1035c0d8a7f335d8aca98ead78c
John Oliver <1615532+johnoliver@users.noreply.github.com>
2021-08-04 03:40:51 +0000

    Fix vendor filter not showing semeru builds

--- adoptopenjdk-frontend-parent/adoptopenjdk-api-v3-frontend/src/main/kotlin/net/adoptopenjdk/api/v3/filters/ReleaseFilter.kt
+++ adoptopenjdk-frontend-parent/adoptopenjdk-api-v3-frontend/src/main/kotlin/net/adoptopenjdk/api/v3/filters/ReleaseFilter.kt
@@ -26,15 +26,13 @@ class ReleaseFilter(
         }
 
         val vendorFilters = if (Ecosystem.CURRENT == Ecosystem.adoptopenjdk && vendor == Vendor.adoptopenjdk) {
-            // if we are in the adoptopenjdk api, and adoptopenjdk builds are requests, then also include adoptium builds
-            (release.vendor == Vendor.adoptium || release.vendor == Vendor.adoptopenjdk)
-        } else if (
-            Ecosystem.CURRENT == Ecosystem.adoptopenjdk &&
-            jvm_impl == JvmImpl.openj9 &&
-            vendor == Vendor.adoptopenjdk
-        ) {
-            // if the user is requesting an openj9 from adopt, also include IBMs
-            (release.vendor == Vendor.ibm || release.vendor == Vendor.adoptopenjdk)
+            if (jvm_impl == JvmImpl.openj9) {
+                // if the user is requesting an openj9 from adopt, also include IBMs
+                (release.vendor == Vendor.adoptopenjdk || release.vendor == Vendor.ibm)
+            } else {
+                // if we are in the adoptopenjdk api, and adoptopenjdk builds are requests, then also include adoptium builds
+                (release.vendor == Vendor.adoptium || release.vendor == Vendor.adoptopenjdk)
+            }
         } else {
             (vendor == null || release.vendor == vendor)
         }
